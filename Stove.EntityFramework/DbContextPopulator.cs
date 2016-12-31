11cdfc472788348f346d561ed6e92b2f7b5474c8
osoykan <oguzhansoykan@gmail.com>
2016-12-31 03:16:06 +0300

    ca

---
+++ Stove.EntityFramework/DbContextPopulator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Autofac;
+
+using FluentAssemblyScanner;
+
+using Stove.EntityFramework.EntityFramework;
+
+namespace Stove.EntityFramework
+{
+    public class DbContextPopulator : IStartable
+    {
+        private readonly IDbContextTypeMatcher _dbContextTypeMatcher;
+
+        public DbContextPopulator(IDbContextTypeMatcher dbContextTypeMatcher)
+        {
+            _dbContextTypeMatcher = dbContextTypeMatcher;
+        }
+
+        public void Start()
+        {
+            Type[] dbContextTypes = AssemblyScanner.FromThisAssembly()
+                                                   .BasedOn<StoveDbContext>()
+                                                   .Filter()
+                                                   .Classes()
+                                                   .NonStatic()
+                                                   .Scan().ToArray();
+
+            _dbContextTypeMatcher.Populate(dbContextTypes);
+        }
+    }
+}
