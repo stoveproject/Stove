1362ba8a9a3d4e3f855ea2ec9024f163b52ae7c7
osoykan <oguzhansoykan@gmail.com>
2017-01-05 01:50:16 +0300

    unit tests added

---
+++ test/Stove.Tests/Threading/AsyncHelper_Tests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+using Shouldly;
+
+using Stove.Threading;
+
+using Xunit;
+
+namespace Stove.Tests.Threading
+{
+    public class AsyncHelper_Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            AsyncHelper.RunSync(AsyncMethod1);
+            AsyncHelper.RunSync(() => AsyncMethod2(21)).ShouldBe(42);
+        }
+
+        private async Task AsyncMethod1()
+        {
+            await Task.Delay(10);
+        }
+
+        private async Task<int> AsyncMethod2(int p)
+        {
+            await Task.Delay(10);
+            return p * 2;
+        }
+    }
+}
---
+++ test/Stove.Tests/Threading/LockExtensions_Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+using Shouldly;
+
+using Stove.Threading.Extensions;
+
+using Xunit;
+
+namespace Stove.Tests.Threading
+{
+    public class LockExtensions_Tests
+    {
+        private readonly List<int> _list;
+
+        public LockExtensions_Tests()
+        {
+            _list = new List<int> { 1 };
+        }
+
+        [Fact]
+        public void Test_Locking()
+        {
+            //Just sample usages:
+            _list.Locking(() => { });
+            _list.Locking(list => { });
+            _list.Locking(() => 42).ShouldBe(42);
+            _list.Locking(list => 42).ShouldBe(42);
+        }
+    }
+}
