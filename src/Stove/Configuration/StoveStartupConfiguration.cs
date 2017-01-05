824725c7432a72e92c8bcc0e98b047489872be55
osoykan <oguzhansoykan@gmail.com>
2017-01-05 03:21:24 +0300

    configuration fix

--- src/Stove/Configuration/StoveStartupConfiguration.cs
+++ src/Stove/Configuration/StoveStartupConfiguration.cs
@@ -1,24 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 
-using Autofac;
 using Autofac.Extras.IocManager;
 
 using Stove.Domain.Uow;
 
 namespace Stove.Configuration
 {
-    public class StoveStartupConfiguration : DictionaryBasedConfig, IStoveStartupConfiguration, IStartable
+    public class StoveStartupConfiguration : DictionaryBasedConfig, IStoveStartupConfiguration
     {
         public StoveStartupConfiguration(IResolver resolver)
         {
             Resolver = resolver;
             TypedConnectionStrings = new Dictionary<Type, string>();
-        }
-
-        public void Start()
-        {
-            UnitOfWork.RegisterFilter(StoveDataFilters.SoftDelete, true);
         }
 
         public IResolver Resolver { get; }
