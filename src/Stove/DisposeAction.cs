daecad44ba378feccfcae6cdb45f87dcf834cff9
Oguzhan Soykan <oguzhan.soykan@trendyol.com>
2017-01-20 09:42:28 +0300

    disposeaction enhancement

--- src/Stove/DisposeAction.cs
+++ src/Stove/DisposeAction.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Threading;
 
 namespace Stove
 {
+    /// <summary>
+    ///     https://referencesource.microsoft.com/#system.web/Util/DisposableAction.cs,e705cb82d7fc864f
+    /// </summary>
+    /// <seealso cref="System.IDisposable" />
     public class DisposeAction : IDisposable
     {
-        private readonly Action _action;
+        public static readonly DisposeAction Null = new DisposeAction(null);
+        private Action _action;
 
         /// <summary>
         ///     Creates a new <see cref="DisposeAction" /> object.
@@ -12,17 +18,14 @@ namespace Stove
         /// <param name="action">Action to be executed when this object is disposed.</param>
         public DisposeAction(Action action)
         {
-            if (action == null)
-            {
-                throw new ArgumentNullException(nameof(action));
-            }
-
             _action = action;
         }
 
         public void Dispose()
         {
-            _action();
+            // Interlocked allows the continuation to be executed only once
+            Action continuation = Interlocked.Exchange(ref _action, null);
+            continuation?.Invoke();
         }
     }
 }
