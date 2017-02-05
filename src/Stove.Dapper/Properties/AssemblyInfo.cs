0367ca0a20550e1e9112d45da5175cd3ace4b248
osoykan <oguzhansoykan@gmail.com>
2017-02-05 15:21:47 +0300

    internalvisibleto fix

--- src/Stove.Dapper/Properties/AssemblyInfo.cs
+++ src/Stove.Dapper/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -20,4 +19,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("5d02a99b-2e08-44c5-83fb-f607afa2b6ff")]
-[assembly: InternalsVisibleTo("Stove.Demo.ConsoleApp")]
