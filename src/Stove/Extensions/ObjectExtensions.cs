e38ff6b63a9f2af721d746fe7ef2f6bdad815c10
osoykan <oguzhansoykan@gmail.com>
2017-02-15 21:37:55 +0300

    refactor for efunitofwork

--- src/Stove/Extensions/ObjectExtensions.cs
+++ src/Stove/Extensions/ObjectExtensions.cs
@@ -1,47 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+
+using JetBrains.Annotations;
 
 namespace Stove.Extensions
 {
     /// <summary>
-    /// Extension methods for all objects.
+    ///     Extension methods for all objects.
     /// </summary>
     public static class ObjectExtensions
     {
         /// <summary>
-        /// Used to simplify and beautify casting an object to a type. 
+        ///     Used to simplify and beautify casting an object to a type.
         /// </summary>
         /// <typeparam name="T">Type to be casted</typeparam>
         /// <param name="obj">Object to cast</param>
         /// <returns>Casted object</returns>
-        public static T As<T>(this object obj)
+        [NotNull]
+        public static T As<T>([NotNull] this object obj)
             where T : class
         {
             return (T)obj;
         }
 
         /// <summary>
-        /// Converts given object to a value type using <see cref="Convert.ChangeType(object,System.TypeCode)"/> method.
+        ///     Converts given object to a value type using <see cref="Convert.ChangeType(object,System.TypeCode)" /> method.
         /// </summary>
         /// <param name="obj">Object to be converted</param>
         /// <typeparam name="T">Type of the target object</typeparam>
         /// <returns>Converted object</returns>
-        public static T To<T>(this object obj)
+        public static T To<T>([NotNull] this object obj)
             where T : struct
         {
             return (T)Convert.ChangeType(obj, typeof(T), CultureInfo.InvariantCulture);
         }
 
         /// <summary>
-        /// Check if an item is in a list.
+        ///     Check if an item is in a list.
         /// </summary>
         /// <param name="item">Item to check</param>
         /// <param name="list">List of items</param>
         /// <typeparam name="T">Type of the items</typeparam>
-        public static bool IsIn<T>(this T item, params T[] list)
+        public static bool IsIn<T>(this T item, [NotNull] params T[] list)
         {
             return list.Contains(item);
+        }
+
+        /// <summary>
+        ///     Fors the each.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items">The items.</param>
+        /// <param name="action">The action.</param>
+        public static void ForEach<T>([CanBeNull] this IEnumerable<T> items, [NotNull] Action<T> action)
+        {
+            if (items == null)
+            {
+                return;
+            }
+
+            foreach (T obj in items)
+            {
+                action(obj);
+            }
         }
     }
 }
