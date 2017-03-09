3310b18f5d4c8cd1a09bd7c5c4149d940fb4a3d8
osoykan <oguzhansoykan@gmail.com>
2017-03-09 23:57:38 +0300

    Stove.Dapper.Tests added

---
+++ test/Stove.Dapper.Tests/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿using Stove.Domain.Entities;
+
+namespace Stove.Dapper.Tests.Entities
+{
+    public class User : Entity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ test/Stove.Dapper.Tests/StoveDapperTestBootstrapper.cs
@@ -0,0 +1,11 @@
+﻿using Stove.Bootstrapping;
+
+namespace Stove.Dapper.Tests
+{
+    [DependsOn(
+        typeof(StoveDapperBootstrapper)
+        )]
+    public class StoveDapperTestBootstrapper : StoveBootstrapper
+    {
+    }
+}
