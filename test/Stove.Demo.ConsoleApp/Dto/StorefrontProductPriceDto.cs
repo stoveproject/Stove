b27a180e467e682756caff87f29ea7964a61fd81
Ayberk Cantürk <ayberk.canturk@trendyol.com>
2017-08-08 16:38:49 +0300

    Create Table Value Parameter Query & Execute method for IDapperRepository<> #58

---
+++ test/Stove.Demo.ConsoleApp/Dto/StorefrontProductPriceDto.cs
@@ -0,0 +1,19 @@
+﻿using Stove.Demo.ConsoleApp.Entities;
+using Stove.Mapster;
+
+namespace Stove.Demo.ConsoleApp.Dto
+{
+    [AutoMap(typeof(StorefrontProductPrice))]
+    public class StorefrontProductPriceDto
+    {
+        public int ProductId { get; set; }
+
+        public int StorefrontId { get; set; }
+
+        public int CurrencyId { get; set; }
+
+        public decimal Msrp { get; set; }
+
+        public decimal SellingPrice { get; set; }
+    }
+}
