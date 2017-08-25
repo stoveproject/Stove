a19362193e2f92a64aa2112e90114f6347bbc4d8
baris.akan@gmail.com <sallama12>
2017-08-25 10:35:11 +0300

    added inlinedata tests for int and double

--- test/Stove.Tests/Extensions/ComparableExtensions_Tests.cs
+++ test/Stove.Tests/Extensions/ComparableExtensions_Tests.cs
@@ -25,5 +25,89 @@ namespace Stove.Tests.Extensions
             dateTimeValue.IsBetween(new DateTime(2014, 1, 1), new DateTime(2015, 1, 1)).ShouldBe(true);
             dateTimeValue.IsBetween(new DateTime(2015, 1, 1), new DateTime(2016, 1, 1)).ShouldBe(false);
         }
+
+        [Theory]
+        [InlineData(5,1,5)]        
+        [InlineData(5, 1, int.MaxValue)]
+        [InlineData(5, 5, int.MaxValue)]
+        [InlineData(5, 5, 5)]        
+        [InlineData(5, int.MinValue, int.MaxValue)]
+        public void Integer_given_positive_integers_within_range(int val,int min, int max)
+        {
+            val.IsBetween(min, max).ShouldBe(true);
+        }
+
+        [Theory]
+        [InlineData(6, int.MinValue, 5)]
+        [InlineData(6, 7, int.MaxValue)]
+        [InlineData(6, int.MaxValue, int.MaxValue)]
+        public void Integer_given_positive_integers_outof_range(int val, int min, int max)
+        {
+            val.IsBetween(min, max).ShouldBe(false);
+        }
+
+
+        [Theory]
+        [InlineData(-5, -5, -5)]
+        [InlineData(-5, int.MinValue, -5)]
+        [InlineData(-5, int.MinValue, int.MaxValue)]
+
+        public void Integer_given_negative_integers_within_range(int val, int min, int max)
+        {
+            val.IsBetween(min, max).ShouldBe(true);
+        }
+
+        [Theory]
+        [InlineData(-10, int.MinValue, int.MinValue)]
+        [InlineData(-10, int.MinValue, -11)]
+        [InlineData(-10, -9, int.MaxValue)]
+        public void Integer_given_negative_integers_outof_range(int val, int min, int max)
+        {
+            val.IsBetween(min, max).ShouldBe(false);
+        }
+
+
+        [Theory]
+        [InlineData(5.0001, 1.0001, 5.0001)]
+        [InlineData(5.0001, 1.0001, double.MaxValue)]
+        [InlineData(5.0001, 5.0001, double.MaxValue)]
+        [InlineData(5.0001, 5.0001, 5.0001)]
+        [InlineData(5.0001, double.MinValue, double.MaxValue)]
+        public void Integer_given_positive_doubles_within_range(double val, double min, double max)
+        {
+            val.IsBetween(min, max).ShouldBe(true);
+        }
+
+
+
+        [Theory]
+        [InlineData(6.0001, double.MinValue, 6.0000)]
+        [InlineData(6.0001, 7.00001, double.MaxValue)]
+        [InlineData(6.0001, double.MaxValue, double.MaxValue)]
+        public void Integer_given_positive_doubles_outof_range(double val, double min, double max)
+        {
+            val.IsBetween(min, max).ShouldBe(false);
+        }
+
+
+
+        [Theory]
+        [InlineData(-5.0001, -5.0001, -5.0001)]
+        [InlineData(-5.0001, double.MinValue, -5.0001)]
+        [InlineData(-5.0001, double.MinValue, double.MaxValue)]
+
+        public void Integer_given_negative_doubles_within_range(double val, double min, double max)
+        {
+            val.IsBetween(min, max).ShouldBe(true);
+        }
+
+        [Theory]
+        [InlineData(-10.0001, double.MinValue, double.MinValue)]
+        [InlineData(-10.0001, double.MinValue, -10.0002)]
+        [InlineData(-10.0001, -9.99999, double.MaxValue)]
+        public void Integer_given_negative_doubles_outof_range(double val, double min, double max)
+        {
+            val.IsBetween(min, max).ShouldBe(false);
+        }
     }
 }
