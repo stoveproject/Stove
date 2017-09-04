aad9280d7483803ea120ed34b0a6d8ed7e514f67
osoykan <oguzhansoykan@gmail.com>
2017-09-04 23:46:54 +0300

    webapicore demo updated

--- test/Stove.Demo.WebApi.Core/Startup.cs
+++ test/Stove.Demo.WebApi.Core/Startup.cs
@@ -8,136 +8,147 @@ using Hangfire;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using NLog.Extensions.Logging;
+using NLog.Web;
+
 using Stove.Demo.WebApi.Core.Domain.DbContexts;
 using Stove.EntityFramework;
-using Stove.EntityFrameworkCore.Configuration;
 using Stove.Redis.Configurations;
 using Stove.Reflection.Extensions;
 
 using Swashbuckle.AspNetCore.Swagger;
 
+using ILogger = Stove.Log.ILogger;
+
 namespace Stove.Demo.WebApi.Core
 {
-    public class Startup
-    {
-        private const string RootLifetimeTag = "root";
+	public class Startup
+	{
+		private const string RootLifetimeTag = "root";
 
-        public Startup(IHostingEnvironment env)
-        {
-            IConfigurationBuilder builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", false, true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
-                .AddEnvironmentVariables();
+		public Startup(IHostingEnvironment env)
+		{
+			IConfigurationBuilder builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", false, true)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+				.AddEnvironmentVariables();
 
-            Configuration = builder.Build();
-        }
+			env.ConfigureNLog("NLog.config");
 
-        public IConfigurationRoot Configuration { get; }
+			Configuration = builder.Build();
+		}
 
-        public IRootResolver RootResolver { get; private set; }
+		public IConfigurationRoot Configuration { get; }
 
-        public ILifetimeScope RootScope { get; private set; }
+		public IRootResolver RootResolver { get; private set; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public IServiceProvider ConfigureServices(IServiceCollection services)
-        {
-            services.AddAutofac();
+		public ILifetimeScope RootScope { get; private set; }
 
-            // Add framework services.
-            services.AddMvc().AddControllersAsServices();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public IServiceProvider ConfigureServices(IServiceCollection services)
+		{
+			services.AddAutofac();
 
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "Stove Web Api", Version = "v1" }); });
+			// Add framework services.
+			services.AddMvc().AddControllersAsServices();
 
-            IocBuilder builder = IocBuilder.New;
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            RootResolver = builder.UseStove<StoveWebApiCoreBootstrapper>()
-                                  .UseStoveEntityFrameworkCore(configuration => configuration)
-                                  .AddStoveDbContext<AnimalDbContext>(contextConfiguration =>
-                                  {
-                                      if (contextConfiguration.ExistingConnection != null)
-                                      {
-                                          contextConfiguration.DbContextOptions.UseSqlServer(contextConfiguration.ExistingConnection);
-                                      }
-                                      else
-                                      {
-                                          contextConfiguration.DbContextOptions.UseSqlServer(Configuration.GetConnectionString("Default"));
-                                      }
-                                  })
-                                  .AddStoveDbContext<PersonDbContext>(contextConfiguration =>
-                                  {
-                                      if (contextConfiguration.ExistingConnection != null)
-                                      {
-                                          contextConfiguration.DbContextOptions.UseSqlServer(contextConfiguration.ExistingConnection);
-                                      }
-                                      else
-                                      {
-                                          contextConfiguration.DbContextOptions.UseSqlServer(Configuration.GetConnectionString("Default"));
-                                      }
-                                  })
-                                  .UseStoveDapper()
-                                  .UseStoveTypedConnectionStringResolver()
-                                  .UseStoveEventBus()
-                                  .UseStoveBackgroundJobs()
-                                  .UseStoveMapster()
-                                  .UseStoveHangfire(configuration =>
-                                  {
-                                      configuration.GlobalConfiguration
-                                                   .UseSqlServerStorage(Configuration.GetConnectionString("Default"))
-                                                   .UseNLogLogProvider();
+			services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "Stove Web Api", Version = "v1" }); });
 
-                                      return configuration;
-                                  })
-                                  .UseStoveNLog()
-                                  .UseStoveRabbitMQ(configuration =>
-                                  {
-                                      configuration.HostAddress = "rabbitmq://localhost/";
-                                      configuration.Username = "admin";
-                                      configuration.Password = "admin";
-                                      configuration.QueueName = "NetCore2";
-                                      return configuration;
-                                  })
-                                  .UseStoveRedisCaching(configuration =>
-                                  {
-                                      configuration.ConfigurationOptions
-                                                   .AddEndpoint("127.0.0.1")
-                                                   .SetDefaultDabase(0)
-                                                   .SetConnectionTimeOut(TimeSpan.FromMinutes(5));
+			IocBuilder builder = IocBuilder.New;
 
-                                      return configuration;
-                                  })
-                                  .RegisterServices(r =>
-                                  {
-                                      r.RegisterAssemblyByConvention(typeof(Startup).GetAssembly());
-                                      r.BeforeRegistrationCompleted += (sender, args) => { args.ContainerBuilder.Populate(services); };
-                                  })
-                                  .CreateResolver();
+			RootResolver = builder.UseStove<StoveWebApiCoreBootstrapper>()
+			                      .UseStoveEntityFrameworkCore(configuration => configuration)
+			                      .AddStoveDbContext<AnimalDbContext>(contextConfiguration =>
+			                      {
+				                      if (contextConfiguration.ExistingConnection != null)
+				                      {
+					                      contextConfiguration.DbContextOptions.UseSqlServer(contextConfiguration.ExistingConnection);
+				                      }
+				                      else
+				                      {
+					                      contextConfiguration.DbContextOptions.UseSqlServer(Configuration.GetConnectionString("Default"));
+				                      }
+			                      })
+			                      .AddStoveDbContext<PersonDbContext>(contextConfiguration =>
+			                      {
+				                      if (contextConfiguration.ExistingConnection != null)
+				                      {
+					                      contextConfiguration.DbContextOptions.UseSqlServer(contextConfiguration.ExistingConnection);
+				                      }
+				                      else
+				                      {
+					                      contextConfiguration.DbContextOptions.UseSqlServer(Configuration.GetConnectionString("Default"));
+				                      }
+			                      })
+			                      .UseStoveDapper()
+			                      .UseStoveTypedConnectionStringResolver()
+			                      .UseStoveEventBus()
+			                      .UseStoveBackgroundJobs()
+			                      .UseStoveMapster()
+			                      .UseStoveHangfire(configuration =>
+			                      {
+				                      configuration.GlobalConfiguration
+				                                   .UseSqlServerStorage(Configuration.GetConnectionString("Default"))
+				                                   .UseNLogLogProvider();
 
-            return new AutofacServiceProvider(RootResolver.Container);
-        }
+				                      return configuration;
+			                      })
+			                      .UseStoveNLog()
+			                      .UseStoveRabbitMQ(configuration =>
+			                      {
+				                      configuration.HostAddress = "rabbitmq://localhost/";
+				                      configuration.Username = "admin";
+				                      configuration.Password = "admin";
+				                      configuration.QueueName = "NetCore2";
+				                      return configuration;
+			                      })
+			                      .UseStoveRedisCaching(configuration =>
+			                      {
+				                      configuration.ConfigurationOptions
+				                                   .AddEndpoint("127.0.0.1")
+				                                   .SetDefaultDabase(0)
+				                                   .SetConnectionTimeOut(TimeSpan.FromMinutes(5));
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+				                      return configuration;
+			                      })
+			                      .RegisterServices(r =>
+			                      {
+				                      r.RegisterAssemblyByConvention(typeof(Startup).GetAssembly());
+				                      r.BeforeRegistrationCompleted += (sender, args) => { args.ContainerBuilder.Populate(services); };
+			                      })
+			                      .CreateResolver();
 
-            app.UseMvc();
+			RootResolver.Resolve<ILogger>().Error("anan");
+			return new AutofacServiceProvider(RootResolver.Container);
+		}
 
-            app.UseSwagger();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
+		{
+			loggerFactory.AddNLog();
 
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Stove WebApi Core"); });
+			app.AddNLogWeb();
 
-            applicationLifetime.ApplicationStopped.Register(() =>
-            {
-                RootScope.Dispose();
-                RootResolver.Dispose();
-            });
-        }
-    }
+			app.UseMvc();
+
+			app.UseSwagger();
+
+			app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Stove WebApi Core"); });
+
+			applicationLifetime.ApplicationStopped.Register(() =>
+			{
+				RootScope.Dispose();
+				RootResolver.Dispose();
+			});
+		}
+	}
 }
