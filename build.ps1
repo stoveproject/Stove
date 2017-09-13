f693564df57508b2afab7cb74a916c8aa4eeabd4
Oğuzhan Soykan <oguzhansoykan@gmail.com>
2017-09-13 15:09:38 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -31,7 +31,7 @@ Param(
     [string[]]$ScriptArgs
 )
 
-$CakeVersion = "0.22.0"
+$CakeVersion = "0.21.1"
 $DotNetChannel = "preview";
 $DotNetVersion = "2.0.0";
 $DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
