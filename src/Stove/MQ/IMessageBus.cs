6565c362b1cd3841b4fed80816696f3a6055c5d4
Oguzhan Soykan <oguzhan.soykan@trendyol.com>
2018-01-25 17:55:05 +0300

    MessageBus Publish methods converted to Task

--- src/Stove/MQ/IMessageBus.cs
+++ src/Stove/MQ/IMessageBus.cs
@@ -5,13 +5,13 @@ namespace Stove.MQ
 {
     public interface IMessageBus
     {
-        void Publish<TMessage>(TMessage message) where TMessage : class;
+        Task Publish<TMessage>(TMessage message) where TMessage : class;
 
-        void Publish<TMessage>(object message) where TMessage : class;
+        Task Publish<TMessage>(object message) where TMessage : class;
 
-        void Publish(object message);
+        Task Publish(object message);
 
-        void Publish(object message, Type messageType);
+        Task Publish(object message, Type messageType);
 
         Task<TResponse> CallRequest<TRequest, TResponse>(TRequest request, TimeSpan timeout, string queueName) where TRequest : class where TResponse : class;
 
--- src/Stove/MQ/NullMessageBus.cs
+++ src/Stove/MQ/NullMessageBus.cs
@@ -7,21 +7,25 @@ namespace Stove.MQ
 	{
 		public static readonly NullMessageBus Instance = new NullMessageBus();
 
-		public void Publish<TMessage>(TMessage message) where TMessage : class
-		{
-		}
+	    public Task Publish<TMessage>(TMessage message) where TMessage : class
+	    {
+	        return null;
+	    }
 
-		public void Publish<TMessage>(object message) where TMessage : class
-		{
-		}
+	    public Task Publish<TMessage>(object message) where TMessage : class
+	    {
+	        return null;
+	    }
 
-		public void Publish(object message)
-		{
-		}
+	    public Task Publish(object message)
+	    {
+	        return null;
+	    }
 
-		public void Publish(object message, Type messageType)
-		{
-		}
+	    public Task Publish(object message, Type messageType)
+	    {
+	        return null;
+	    }
 
 	    public Task<TResponse> CallRequest<TRequest, TResponse>(TRequest request, TimeSpan timeout, string queueName) where TRequest : class where TResponse : class
 	    {
