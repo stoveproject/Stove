58eee64b84a9f15ab99ded2e266a25a6f89b0aca
Oguzhan Soykan <o.soykan@evision-software.com>
2018-09-26 16:30:48 +0200

    netcoreapp version bumpled on tests

--- build.ps1
+++ build.ps1
@@ -31,9 +31,9 @@ Param(
     [string[]]$ScriptArgs
 )
 
-$CakeVersion = "0.27.2"
+$CakeVersion = "0.30.0"
 $DotNetChannel = "LTS";
-$DotNetVersion = "2.1.200";
+$DotNetVersion = "2.1.4";
 $DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 
